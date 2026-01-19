fd2ad1fdda8831f81fcb93f7796a1674b8d4bf55
Yovio-del <yovinsolomon@gmail.com>
2026-01-18 20:04:20 -0500

    Fixed Errors Again

---
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,30 @@
+import dayjs from 'dayjs';
+import { CartItemDetails } from './CartItemDetails';
+import { DeliveryOptions } from './DeliveryOptions';
+
+
+export function OrderSummary({ cart, deliveryOptions }) {
+  return (
+    <div className="order-summary">
+      {deliveryOptions.length > 0 && cart.map((cartItem) => {
+        const selectedDeliveryOption = deliveryOptions
+          .find((deliveryOption) => {
+            return deliveryOption.id === cartItem.deliveryOptionId;
+          });
+        return (
+          <div key={cartItem.productId} className="cart-item-container">
+            <div className="delivery-date">
+              Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+            </div>
+
+            <div className="cart-item-details-grid">
+               <CartItemDetails cartItem={cartItem} />
+
+              <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
