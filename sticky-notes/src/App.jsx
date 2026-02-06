62e40f16a5e05b7e19b73feb6223777082e40ea5
Yovio-del <yovinsolomon@gmail.com>
2026-02-06 08:12:32 -0500

    created the ability to make a title for a note

--- sticky-notes/src/App.jsx
+++ sticky-notes/src/App.jsx
@@ -1,34 +1,23 @@
 import './App.css'
-
+import { Footer } from './Footer'
+import { Notes } from './Notes'
 function App() {
-  
+
 
   return (
-    <body>
+    <main>
       <header>
         <p className='title'>Sticky Notes Extension - MVP Check list</p>
       </header>
 
       <hr />
 
-      <div className='notes'>
-        <ol>
-          <li>Create a note(text only)</li>
-          <li>See a list of saved notes</li>
-          <li>Click a not of saved notes</li>
-          <li>No folders, no tags, no search</li>
-          <li>Stick Shows the note text </li>
-        </ol>
-      </div>
+      <Notes />
 
       <hr />
 
-      <footer>
-        <button className='note-creator-button'>+ New Note</button>
-        <input type="text" placeholder='Type note text here...' 
-        className='note-text'/>
-      </footer>
-    </body>
+      <Footer />
+    </main>
   )
 }
 
---
+++ sticky-notes/src/Footer.jsx
@@ -0,0 +1,43 @@
+import './Footer.css'
+import { useState, useEffect } from 'react'
+
+export function Footer() {
+
+    const [notes, setNotes] = useState([])
+    const [input, setInput] = useState("")
+
+    const getInput = (event) => {
+        setInput(event.target.value)
+    }
+
+    const addTask = () => {
+        if (input !== "") {
+            setNotes([...notes, {
+                id: crypto.randomUUID(),
+                title: input
+            }])
+        }
+
+    }
+
+    const EnterKey = (event) => {
+        if (event.key === "Enter") {
+            addTask();
+        }
+    }
+
+
+    useEffect(() => {
+        console.log(notes)
+    }, [notes])
+
+
+    return (
+        <footer>
+            <button className='note-creator-button' onClick={addTask}>+ New Note</button>
+            <input type="text" placeholder='Create Title here...'
+                className='note-text'
+                value={input} onChange={getInput} onKeyDown={EnterKey} />
+        </footer>
+    )
+}
