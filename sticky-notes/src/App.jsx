3eacbdd6fcce438787e63eb3ecf016c0dceb3b31
Yovio-del <yovinsolomon@gmail.com>
2026-03-11 19:01:31 -0400

    successfully uploaded the extension to google chrome

--- sticky-notes/src/App.jsx
+++ sticky-notes/src/App.jsx
@@ -1,24 +1,26 @@
-import './App.css'
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { FrontPage } from './front-note-page/FrontPage'
+import { FrontPage } from './front-note-page/FrontPage';
 import { AtomicNote } from './single-note-page/AtomicNote';
 import { useEffect, useState } from 'react';
-function App() {
-  const [notes, setNotes] = useState(() => {
-        return JSON.parse(localStorage.getItem('notes')) || []
-    
-    });
 
-    useEffect(() => {
-        localStorage.setItem('notes',JSON.stringify(notes))
-    },[notes])
+function App() {
+  // Move the logic from content-main.jsx here
+  const [notes, setNotes] = useState(() => {
+    return JSON.parse(localStorage.getItem('notes')) || [];
+  });
+
+  useEffect(() => {
+    // Save notes to localStorage whenever they change
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <Routes>
-      <Route path="/" element={<FrontPage notes={notes} setNotes={setNotes}/>} />
-      <Route path='/note/:id' element={<AtomicNote notes={notes} setNotes={setNotes}/>}/>
+      <Route path="/" element={<FrontPage notes={notes} setNotes={setNotes} />} />
+      <Route path="/note/:id" element={<AtomicNote notes={notes} setNotes={setNotes} />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
